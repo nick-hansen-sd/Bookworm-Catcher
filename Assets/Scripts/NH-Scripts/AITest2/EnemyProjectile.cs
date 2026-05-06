9f6f6368641bee2a3ed880660354170ee63fee61
Lyn-Solarn <lalarson2003@gmail.com>
2026-05-05 19:24:17 -0700

    Revert "Added Particles and Audio"

--- Assets/Scripts/NH-Scripts/AITest2/EnemyProjectile.cs
+++ Assets/Scripts/NH-Scripts/AITest2/EnemyProjectile.cs
@@ -2,18 +2,8 @@ using UnityEngine;
 
 public class EnemyProjectile : MonoBehaviour
 {
-    //-----------Moises-------------
-    [SerializeField] ParticleSystem projectileCollision;
-    //------------------------------
-    
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //-----------Moises-------------
-        //Instantaites particle system at collision location and then destroys particle system after done playing its final animation
-        ParticleSystem particleSystem = Instantiate(projectileCollision, gameObject.transform.position, Quaternion.identity);
-        Destroy(particleSystem.gameObject, particleSystem.main.duration + particleSystem.main.startLifetime.constantMax);
-        //------------------------------
-
         Destroy(gameObject);
     }
 }
